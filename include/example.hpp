b839f6606d81e68d0d58612e729ff59d174ec54f
TessaHartmann <74740688+TessaHartmann@users.noreply.github.com>
2021-01-21 09:42:31 -0500

    Update example.hpp
    File fix of example.hpp

--- include/example.hpp
+++ include/example.hpp
@@ -1,10 +1,10 @@
-/*
-  !\file example.hpp
-  \breif use of string substring and loops
+/*! \file example.hpp
+  	\breif use of string substring and loops
   
   A string is a variable which stores a line of characters, which can either be set to a certain limit.
   A substring is a section of a string that is allowed to start at an index and run a certain length.
   Loops are defined to run through a certain amount of iterations set by either a for loop, a do-while loop or a while loop.
+   \bug	   No known bugs.
 */
 
 #ifndef EXAMPLE_hpp
@@ -14,3 +14,4 @@
 int globalvar = 0;
 
 #endif 
+
